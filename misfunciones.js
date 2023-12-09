e6ef134d603dd2ab5a0fd5a681a3775784f66c4e
JonnyIvan24 <johnny.perez.u@gmail.com>
2023-12-09 10:26:59 -0600

    JS - eventos

---
+++ misfunciones.js
@@ -0,0 +1,12 @@
+function duplicar() {
+    let valorOriginal = document.getElementById('original').value
+    document.getElementById('copia').value = valorOriginal
+}
+
+function mOver(elemento) {
+    elemento.innerHTML = "Slayer"
+}
+
+function mOut(elemento) {
+    elemento.innerHTML = "Doom"
+}
